a4a761486d9ede526b08802a061c3d0fdff0f78c
ymartinguzman <yamiramartinguzman@gmail.com>
2020-11-23 17:33:16 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../stylesheets/App.scss';
+import CharacterList from './CharacterList';
+
+function App() {
+  return (
+    <div>
+      <header></header>
+      <main>
+        <CharacterList />
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
