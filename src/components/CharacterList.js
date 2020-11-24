4ce84797666c7870b84180a5d3a385cef9b29195
ymartinguzman <yamiramartinguzman@gmail.com>
2020-11-24 13:02:02 +0100

    add picture and text when results not found

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
 import { Link } from 'react-router-dom';
+import image from '../images/noresults.jpg';
 
 const CharacterList = (props) => {
-  console.log(props.characters.id);
-
-  if (props.characters.id === undefined) {
-    return <p>Character not found</p>;
+  if (props.characters.length === 0) {
+    return (
+      <div>
+        <p>No results found!</p>
+        <img
+          src={image}
+          alt="No results founded"
+          title="No results founded"
+        ></img>
+      </div>
+    );
   }
   const html = props.characters.map((item) => (
     <li key={item.id}>
@@ -21,6 +29,7 @@ const CharacterList = (props) => {
       />
     </li>
   ));
+
   return (
     <>
       <ul>{html}</ul>
