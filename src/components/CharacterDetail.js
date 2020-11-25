6cd145da1d166cd9636e66fd283d95849ec8d5dc
ymartinguzman <yamiramartinguzman@gmail.com>
2020-11-25 12:37:12 +0100

    fit objects to mobile responsive

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -5,19 +5,24 @@ const CharacterDetail = (props) => {
   return (
     <>
       <div className="card-container">
-        <img src={props.image} alt={props.name} title={props.name}></img>
+        <img
+          className="image-card"
+          src={props.image}
+          alt={props.name}
+          title={props.name}
+        ></img>
         <div className="card-details">
-          <h2> {props.name}</h2>
-          <p>
+          <h2 className="card-text"> {props.name}</h2>
+          <p className="card-text">
             <strong>Status: </strong>Status: {props.status}
           </p>
-          <p>
+          <p className="card-text">
             <strong>SPECIES: </strong> {props.species}
           </p>
-          <p>
+          <p className="card-text">
             <strong>ORIGIN: </strong> {props.origin}
           </p>
-          <p>
+          <p className="card-text">
             <strong>EPISODES: </strong> {props.episodes}
           </p>
         </div>
